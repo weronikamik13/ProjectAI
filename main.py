576950b7714a27c2ffe5d9942e64c86785f893b1
weronikamik13 <wero1996@onet.eu>
2020-04-19 21:37:23 +0200

    adding main

--- main.py
+++ main.py
@@ -1,52 +1,72 @@
 from easyAI import TwoPlayersGame
-from easyAI.Player import Human_Player
+
+# Convert D7 to (3,6) and back...
+to_string = lambda move: " ".join(["ABCDEFGHIJ"[move[i][0]] + str(move[i][1] + 1)
+                                   for i in (0, 1)])
+to_tuple = lambda s: ("ABCDEFGHIJ".index(s[0]), int(s[1:]) - 1)
 
 
-class TicTacToe(TwoPlayersGame):
-    """ The board positions are numbered as follows:
-            7 8 9
-            4 5 6
-            1 2 3
+class Hexapawn(TwoPlayersGame):
+    """
+    A nice game whose rules are explained here:
+    http://fr.wikipedia.org/wiki/Hexapawn
     """
 
-    def __init__(self, players):
+    def __init__(self, players, size=(4, 4)):
+        self.size = M, N = size
+        p = [[(i, j) for j in range(N)] for i in [0, M - 1]]
+
+        for i, d, goal, pawns in [(0, 1, M - 1, p[0]), (1, -1, 0, p[1])]:
+            players[i].direction = d
+            players[i].goal_line = goal
+            players[i].pawns = pawns
+
         self.players = players
-        self.board = [0 for i in range(9)]
-        self.nplayer = 1  # player 1 starts.
+        self.nplayer = 1
 
     def possible_moves(self):
-        return [i + 1 for i, e in enumerate(self.board) if e == 0]
+        moves = []
+        opponent_pawns = self.opponent.pawns
+        d = self.player.direction
+        for i, j in self.player.pawns:
+            if (i + d, j) not in opponent_pawns:
+                moves.append(((i, j), (i + d, j)))
+            if (i + d, j + 1) in opponent_pawns:
+                moves.append(((i, j), (i + d, j + 1)))
+            if (i + d, j - 1) in opponent_pawns:
+                moves.append(((i, j), (i + d, j - 1)))
+
+        return list(map(to_string, [(i, j) for i, j in moves]))
 
     def make_move(self, move):
-        self.board[int(move) - 1] = self.nplayer
+        move = list(map(to_tuple, move.split(' ')))
+        ind = self.player.pawns.index(move[0])
+        self.player.pawns[ind] = move[1]
 
-    def unmake_move(self, move):  # optional method (speeds up the AI)
-        self.board[int(move) - 1] = 0
+        if move[1] in self.opponent.pawns:
+            self.opponent.pawns.remove(move[1])
 
     def lose(self):
-        """ Has the opponent "three in line ?" """
-        return any([all([(self.board[c - 1] == self.nopponent)
-                         for c in line])
-                    for line in [[1, 2, 3], [4, 5, 6], [7, 8, 9],  # horiz.
-                                 [1, 4, 7], [2, 5, 8], [3, 6, 9],  # vertical
-                                 [1, 5, 9], [3, 5, 7]]])  # diagonal
+        return (any([i == self.opponent.goal_line
+                     for i, j in self.opponent.pawns])
+                or (self.possible_moves() == []))
 
     def is_over(self):
-        return (self.possible_moves() == []) or self.lose()
+        return self.lose()
 
     def show(self):
-        print('\n' + '\n'.join([
-            ' '.join([['.', 'O', 'X'][self.board[3 * j + i]]
-                      for i in range(3)])
-            for j in range(3)]))
-
-    def scoring(self):
-        return -100 if self.lose() else 0
+        f = lambda x: '1' if x in self.players[0].pawns else (
+            '2' if x in self.players[1].pawns else '.')
+        print("\n".join([" ".join([f((i, j))
+                                   for j in range(self.size[1])])
+                         for i in range(self.size[0])]))
 
 
 if __name__ == "__main__":
-    from easyAI import AI_Player, Negamax
+    from easyAI import AI_Player, Human_Player, Negamax
 
-    ai_algo = Negamax(6)
-    TicTacToe([Human_Player(), AI_Player(ai_algo)]).play()
-
+    scoring = lambda game: -100 if game.lose() else 0
+    ai = Negamax(10, scoring)
+    game = Hexapawn([AI_Player(ai), AI_Player(ai)])
+    game.play()
+    print("player %d wins after %d turns " % (game.nopponent, game.nmove))
